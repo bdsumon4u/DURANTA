522ac1e8dc1080c7a043d5bc892b00df451b16d1
Sumon Ahmed <bdsumon4u@gmail.com>
2021-08-26 00:39:05 +0600

    ServiceFeature BottomOfSlider

--- resources/js/Components/Site/Slider.vue
+++ resources/js/Components/Site/Slider.vue
@@ -8,6 +8,42 @@
                 :title="slide.title"
                 :content="slide.content" />
         </vueper-slides>
+        <!-- features -->
+        <div class="pt-5">
+            <div class="grid md:grid-cols-3 gap-3 lg:gap-6 mx-auto justify-center">
+                <!-- single feature -->
+                <div
+                    class="border-primary border rounded-sm px-8 lg:px-3 lg:py-6 py-4 flex justify-center items-center gap-5">
+                    <img src="/images/icons/delivery-van.svg" class="lg:w-12 w-10 h-12 object-contain">
+                    <div>
+                        <h4 class="font-medium capitalize text-lg">Delivery</h4>
+                        <p class="text-gray-500 text-xs lg:text-sm">Very fast shipping</p>
+                    </div>
+                </div>
+                <!-- single feature end -->
+                <!-- single feature -->
+                <div
+                    class="border-primary border rounded-sm px-8 lg:px-3 lg:py-6 py-4 flex justify-center items-center gap-5">
+                    <img src="/images/icons/money-back.svg" class="lg:w-12 w-10 h-12 object-contain">
+                    <div>
+                        <h4 class="font-medium capitalize text-lg">Refund</h4>
+                        <p class="text-gray-500 text-xs lg:text-sm">30 Days money return</p>
+                    </div>
+                </div>
+                <!-- single feature end -->
+                <!-- single feature -->
+                <div
+                    class="border-primary border rounded-sm px-8 lg:px-3 lg:py-6 py-4 flex justify-center items-center gap-5">
+                    <img src="/images/icons/service-hours.svg" class="lg:w-12 w-10 h-12 object-contain">
+                    <div>
+                        <h4 class="font-medium capitalize text-lg">Support</h4>
+                        <p class="text-gray-500 text-xs lg:text-sm">24/7 Customer support</p>
+                    </div>
+                </div>
+                <!-- single feature end -->
+            </div>
+        </div>
+        <!-- features end -->
     </div>
 </template>
 
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,7 +4,7 @@
         <div>
             <div class="container">
                 <div class="flex">
-                    <div class="hidden md:block md:w-64">
+                    <div class="hidden md:block md:w-64 mb-4">
                         <category-menu />
                     </div>
                     <div class="flex-1">
@@ -16,43 +16,6 @@
             </div>
         </div>
         <!-- slider end -->
-
-        <!-- features -->
-        <div class="container py-16">
-            <div class="lg:w-10/12 grid md:grid-cols-3 gap-3 lg:gap-6 mx-auto justify-center">
-                <!-- single feature -->
-                <div
-                    class="border-primary border rounded-sm px-8 lg:px-3 lg:py-6 py-4 flex justify-center items-center gap-5">
-                    <img src="/images/icons/delivery-van.svg" class="lg:w-12 w-10 h-12 object-contain">
-                    <div>
-                        <h4 class="font-medium capitalize text-lg">free shipping</h4>
-                        <p class="text-gray-500 text-xs lg:text-sm">Order over $200</p>
-                    </div>
-                </div>
-                <!-- single feature end -->
-                <!-- single feature -->
-                <div
-                    class="border-primary border rounded-sm px-8 lg:px-3 lg:py-6 py-4 flex justify-center items-center gap-5">
-                    <img src="/images/icons/money-back.svg" class="lg:w-12 w-10 h-12 object-contain">
-                    <div>
-                        <h4 class="font-medium capitalize text-lg">Money returns</h4>
-                        <p class="text-gray-500 text-xs lg:text-sm">30 Days money return</p>
-                    </div>
-                </div>
-                <!-- single feature end -->
-                <!-- single feature -->
-                <div
-                    class="border-primary border rounded-sm px-8 lg:px-3 lg:py-6 py-4 flex justify-center items-center gap-5">
-                    <img src="/images/icons/service-hours.svg" class="lg:w-12 w-10 h-12 object-contain">
-                    <div>
-                        <h4 class="font-medium capitalize text-lg">24/7 Support</h4>
-                        <p class="text-gray-500 text-xs lg:text-sm">Customer support</p>
-                    </div>
-                </div>
-                <!-- single feature end -->
-            </div>
-        </div>
-        <!-- features end -->
 
         <!-- shop by brand -->
         <div class="container pb-16">
